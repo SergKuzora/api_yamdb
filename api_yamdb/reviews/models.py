3a1faed3ac009424b6b38cc59f168ca1ac4b7521
Ghost <yasinabdurakhmanov@gmail.com>
2021-09-06 21:27:19 +0300

    create models - Review and - Comments

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import get_user_model
+from Django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 User = get_user_model()
 
 
 class Review(models.Model):
-    pass
+    text = models.CharField(max_length=200)
+    score = models.IntegerField(
+        validators=[MinValueValidator(1),
+                    MaxValueValidator(10)])
 
 
 class Comments(models.Model):
-    pass
+    text = models.CharField(max_length=200)
