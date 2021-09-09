de8e72048d837ad13139e9a8a4f50495073dd5b4
Ghost <yasinabdurakhmanov@gmail.com>
2021-09-09 19:58:48 +0300

    changes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import get_object_or_404
 from reviews.models import Review
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .permissions import IsAuthorOrReadOnlyPermission
 
 from .serializers import CommentsSerializer, ReviewSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = (IsAuthorOrReadOnlyPermission,
+                          IsAuthenticatedOrReadOnly)
+
+    def get_queryset(self):
+        return Review.objects.filter(pk=self.kwargs.get('title_id'))
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -15,6 +22,8 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentsViewSet(viewsets.ModelViewSet):
     serializer_class = CommentsSerializer
+    permission_classes = (IsAuthorOrReadOnlyPermission,
+                          IsAuthenticatedOrReadOnly)
 
     def perform_create(self, serializer):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,17 +5,22 @@ from django.db import models
 User = get_user_model()
 
 
-class Users(models.Model):
-    username = models.CharField(max_lenght=150)
-    email = models.EmailField(max_lenght=254)
-    first_name = models.CharField(max_lenght=150)
-    last_name = models.CharField(max_lenght=150)
-    bio = models.CharField()
-    roles = ['user', 'moderator', 'admin']
-    role = models.CharField(choices=roles, default='user',)
+class Title(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='titles'
+    )
 
 
 class Review(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
     text = models.CharField(max_length=200)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='reviews')
@@ -24,11 +29,16 @@ class Review(models.Model):
                     MaxValueValidator(10)])
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
+    class Meta:
+        ordering = ['score']
+
     def __str__(self):
         return self.text
 
 
 class Comments(models.Model):
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
     text = models.CharField(max_length=200)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments')
