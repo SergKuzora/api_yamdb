72f6ccb8d58d94d6578316e642a83c35593011e7
Ghost <yasinabdurakhmanov@gmail.com>
2021-09-15 19:21:54 +0300

    changes

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -93,9 +93,10 @@ class ReviewSerializer(serializers.ModelSerializer):
         author = self.context['request'].user
         title_id = self.context.get('view').kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
-        return Review.objects.create(
+        data = Review.objects.create(
             title=title, author=author, **validated_data
         )
+        return data
 
     class Meta:
         fields = '__all__'
